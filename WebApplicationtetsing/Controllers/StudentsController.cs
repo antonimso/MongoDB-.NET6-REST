bd3ab1947421e141409d72aa5c6d6e93fa67a683
AntonioMateusSousade <antonio.oliveira@levva.io>
2023-07-24 11:44:22 -0300

    crud with mongodb

---
+++ WebApplicationtetsing/Controllers/StudentsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationtetsing.Models;
+using WebApplicationtetsing.Services;
+
+namespace WebApplicationtetsing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentService studentService;
+
+        public StudentsController(IStudentService studentService)
+        {
+            this.studentService = studentService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Student>> Get()
+        {
+            return studentService.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> Get(string id) 
+        {
+            var student = studentService.Get(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+            return student;
+        }
+
+
+        [HttpPost]
+        public ActionResult<Student> Post(Student student) 
+        {
+            studentService.Create(student);
+            return CreatedAtAction(nameof(Get), new { id = student.Id }, student);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, Student student) {
+        
+            var existingStudent = studentService.Get(id);
+
+            if (existingStudent == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+            studentService.Update(id, existingStudent);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var student = studentService.Get(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+
+            studentService.Remove(student.Id);
+
+            return Ok($"Student with Id = {id} deleted");
+        }
+    }
+}
---
+++ WebApplicationtetsing/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApplicationtetsing.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+
+        [BsonElement("gratuated")]
+        public bool IsGratuated { get; set; }
+
+        [BsonElement("courses")]
+        public string[]? Courses { get; set; }
+
+        [BsonElement("gender")]
+        public string gender { get; set; } = String.Empty;
+
+        [BsonElement("age")]
+        public int Age { get; set; }
+    }
+}
